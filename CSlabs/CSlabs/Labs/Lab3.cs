205dc64eafc465c72ca6296f382c32e8fdad0426
vladPC <vladshmigero@gmail.com>
2025-11-12 21:59:46 +0300

    Классы

--- CSlabs/CSlabs/Labs/Lab3.cs
+++ CSlabs/CSlabs/Labs/Lab3.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
+using static CSlabs.Labs.Lab3;
 
 namespace CSlabs.Labs
 {
@@ -24,10 +26,46 @@ namespace CSlabs.Labs
         }
         public class Sentence
         {
+            public List<Token> Tokens { get; private set; } = new List<Token>();
+            public override string ToString() 
+            {
+                var sb = new System.Text.StringBuilder();
+                foreach (var token in Tokens)
+                {
+                    sb.Append(token.Word);
+                }
+                return sb.ToString();
+            }
+
+
         }
         public class Text
         {
+            public List <Sentence> Sentences { get; private set;} = new List<Sentence>();
+            public override string ToString()
+            {
+                var sb = new StringBuilder();
+                for (int i = 0; i < Sentences.Count; i++)
+                {
+                    sb.Append(Sentences[i].ToString());
+                }
+                return sb.ToString();
+            }
 
+        }
+        class Parser
+        {
+            public static Text Parse(string sequences)
+            {
+                var matches = Regex.Matches(sequences, @"\w+|[.,!?;:]");//регулярка слова или знаки препинания .,!?;:
+                Console.WriteLine("Токены:");
+                foreach (var token in matches)
+                {
+                    Console.WriteLine(token);
+                }
+                var text = new Text();
+                return text;
+            }
         }
 
 
@@ -36,11 +74,8 @@ namespace CSlabs.Labs
         static void Main(string[] args)
         {
             string inputFile = @"C:\Users\user\source\repos\vladshmigero\CSlabs\CSlabs\CSlabs\TextFile.txt";
-            string[] sequences = File.ReadAllLines(inputFile);
-            foreach (string line in sequences)
-            {
-                Console.WriteLine(line);
-            }
+            string sequences = File.ReadAllText(inputFile);
+            Parser.Parse(sequences);
         }
     }
 }
